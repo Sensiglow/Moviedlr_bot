549f231b0a3489eb2cf4f89a4e2f5cc71a105fc3
Sensiglow <Krishbasak878@gmail.com>
2026-04-28 23:56:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import asyncio
 import os
 
+# Render environment variable থেকে TOKEN নেবে
 TOKEN = os.getenv("TOKEN")
 
+# movie key → file_id
 movies = {
     "krish3": "AAMCBQADGQEDDbilafDunOnoxxNtkvWBPMopKcBirIwAAoMeAAIrMIhXtXbv2vuERtkBAAdtAAM7BA"
 }
@@ -13,7 +15,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
 
     if context.args:
-        key = context.args[0]
+        key = context.args[0].lower()
 
         if key in movies:
             msg = await context.bot.send_video(
@@ -22,28 +24,22 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 caption="⏳ File will be removed in 1 hour"
             )
 
+            # 1 hour (3600 sec) পরে delete
             await asyncio.sleep(3600)
-            await context.bot.delete_message(chat_id=user_id, message_id=msg.message_id)
+            await context.bot.delete_message(
+                chat_id=user_id,
+                message_id=msg.message_id
+            )
 
         else:
             await update.message.reply_text("Movie not found ❌")
+
     else:
         await update.message.reply_text("Send valid link")
 
+# app start
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+
+print("Bot is running... 🚀")
 app.run_polling()
-import threading
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Bot is running')
-
-def run_web():
-    server = HTTPServer(('0.0.0.0', 10000), Handler)
-    server.serve_forever()
-
-threading.Thread(target=run_web).start()
